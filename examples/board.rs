6f21379e39cd3f5e29c8f77b583d761378460957
Masaki Wakabayashi <masaki.wakabayashi@gmail.com>
2023-03-05 18:29:40 +0900

    Refine as an example.

--- examples/board.rs
+++ examples/board.rs
@@ -1,7 +1,8 @@
 use life_backend::Board;
 
 fn main() {
-    let board: Board = [(0, 0), (1, 0), (2, 0), (1, 1)].iter().collect();
+    let pattern = [(0, 0), (1, 0), (2, 0), (1, 1)]; // T-tetromino
+    let board: Board = pattern.iter().collect();
     println!("Print as Debug: {:?}", board);
     println!("Print as Display:\n{board}");
 }
