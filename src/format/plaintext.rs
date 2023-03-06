664db3b88821238f20d27c7e6c60864addb45a8c
Masaki Wakabayashi <masaki.wakabayashi@gmail.com>
2023-03-07 03:48:45 +0900

    Introduce an internal struct PlaintextPartial.

--- src/format/plaintext.rs
+++ src/format/plaintext.rs
@@ -1,4 +1,4 @@
-use anyhow::{bail, ensure, Result};
+use anyhow::{bail, Result};
 use std::fmt;
 
 /// A representation for Plaintext file format, described in <https://conwaylife.com/wiki/Plaintext>.
@@ -9,7 +9,78 @@ pub struct Plaintext {
     pattern: Vec<Vec<bool>>,
 }
 
-// Inherent methods
+// An internal struct, used during constructing of Plaintext
+struct PlaintextPartial {
+    name: Option<String>,
+    comment: Vec<String>,
+    pattern: Vec<Vec<bool>>,
+}
+
+// Inherent methods of PlaintextPartial
+
+impl PlaintextPartial {
+    fn parse_prefixed_line<'a>(prefix: &str, line: &'a str) -> Option<&'a str> {
+        if line.len() < prefix.len() {
+            None
+        } else {
+            let (first, last) = line.split_at(prefix.len());
+            if first == prefix {
+                Some(last)
+            } else {
+                None
+            }
+        }
+    }
+    #[inline]
+    fn parse_name_line(line: &str) -> Option<&str> {
+        Self::parse_prefixed_line("!Name: ", line)
+    }
+    #[inline]
+    fn parse_comment_line(line: &str) -> Option<&str> {
+        Self::parse_prefixed_line("!", line)
+    }
+    fn parse_pattern_line(line: &str) -> Option<Vec<bool>> {
+        let mut buf = Vec::new();
+        for char in line.chars() {
+            let value = match char {
+                '.' => false,
+                'O' => true,
+                _ => return None,
+            };
+            buf.push(value);
+        }
+        Some(buf)
+    }
+    fn new() -> Self {
+        Self {
+            name: None,
+            comment: Vec::new(),
+            pattern: Vec::new(),
+        }
+    }
+    fn push(&mut self, line: &str) -> Result<()> {
+        if self.name.is_none() {
+            let Some(name) = Self::parse_name_line(line) else {
+                bail!("The header line is in wrong format");
+            };
+            self.name = Some(name.to_string());
+        } else {
+            if self.pattern.is_empty() {
+                if let Some(comment) = Self::parse_comment_line(line) {
+                    self.comment.push(comment.to_string());
+                    return Ok(());
+                }
+            }
+            let Some(pattern) = Self::parse_pattern_line(line) else {
+                bail!("Invalid character found in the pattern");
+            };
+            self.pattern.push(pattern);
+        }
+        Ok(())
+    }
+}
+
+// Inherent methods of Plaintext
 
 impl Plaintext {
     /// Creates from the specified string.
@@ -32,55 +103,20 @@ impl Plaintext {
     /// ```
     ///
     pub fn new(str: &str) -> Result<Self> {
-        let mut lines = str.lines();
-        let name: String = {
-            let Some(line) = lines.next() else {
-                bail!("No header line in the pattern");
-            };
-            let expected_prefix = "!Name: ";
-            if line.len() < expected_prefix.len() {
-                bail!("The header line is in wrong format");
+        let partial = {
+            let mut buf = PlaintextPartial::new();
+            for line in str.lines() {
+                buf.push(line)?;
             }
-            let (prefix, body) = line.split_at(expected_prefix.len());
-            if prefix != expected_prefix {
-                bail!("The header line is in wrong format");
-            }
-            body.to_string()
+            buf
         };
-        let mut comment = Vec::new();
-        let mut pattern = Vec::new();
-        let mut comment_done = false;
-        for line in lines {
-            if !comment_done {
-                if !line.is_empty() {
-                    let (first, rest) = line.split_at(1);
-                    if first == "!" {
-                        comment.push(rest.to_string());
-                        continue;
-                    }
-                } else {
-                    ensure!(
-                        comment.is_empty(),
-                        "Invalid empty line found in the optional header"
-                    );
-                }
-                comment_done = true;
-            }
-            let mut buf = Vec::new();
-            for char in line.chars() {
-                let value = match char {
-                    '.' => false,
-                    'O' => true,
-                    _ => bail!("Invalid character found in the pattern"),
-                };
-                buf.push(value);
-            }
-            pattern.push(buf);
-        }
+        let Some(name) = partial.name else {
+            bail!("No header line in the pattern");
+        };
         Ok(Self {
             name,
-            comment,
-            pattern,
+            comment: partial.comment,
+            pattern: partial.pattern,
         })
     }
 
