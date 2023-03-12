115c7bade4c7c9f292e9f45b1b9cf09eaa2e3f99
Masaki Wakabayashi <masaki.wakabayashi@gmail.com>
2023-03-12 20:04:06 +0900

    Implement update method.

--- src/game.rs
+++ src/game.rs
@@ -1,7 +1,10 @@
 use super::Board;
-use num_traits::{One, ToPrimitive, Zero};
+use num_iter::range_inclusive;
+use num_traits::{Bounded, One, ToPrimitive, Zero};
+use std::collections::HashSet;
 use std::fmt;
 use std::hash::Hash;
+use std::ops::{Add, Sub};
 
 /// The default index type of boards.
 pub type DefaultIndexType = i16;
@@ -53,6 +56,74 @@ where
     #[inline]
     pub fn board(&self) -> &Board<IndexType> {
         &self.board
+    }
+
+    // Returns positions of neighbourhoods of the specified position, and the specified position itself if requested.
+    fn neighbourhood_plus_center(includes_center: bool, x: IndexType, y: IndexType) -> Vec<(IndexType, IndexType)>
+    where
+        IndexType: Copy + PartialEq + PartialOrd + Add<Output = IndexType> + Sub<Output = IndexType> + One + Bounded + ToPrimitive,
+    {
+        let min = IndexType::min_value();
+        let max = IndexType::max_value();
+        let one = IndexType::one();
+        let mut buf = Vec::new();
+        for v in range_inclusive(if y > min { y - one } else { y }, if y < max { y + one } else { y }) {
+            for u in range_inclusive(if x > min { x - one } else { x }, if x < max { x + one } else { x }) {
+                if includes_center || u != x || v != y {
+                    buf.push((u, v))
+                }
+            }
+        }
+        buf
+    }
+
+    // Returns the next board of the specified board.
+    fn next_board(board: &Board<IndexType>) -> Board<IndexType>
+    where
+        IndexType: Copy + PartialEq + PartialOrd + Add<Output = IndexType> + Sub<Output = IndexType> + One + Bounded + ToPrimitive,
+    {
+        let scan_positions: HashSet<(IndexType, IndexType)> = board.iter().flat_map(|&(x, y)| Self::neighbourhood_plus_center(true, x, y)).collect();
+        let next_board: Board<IndexType> = scan_positions
+            .into_iter()
+            .filter(|&(x, y)| {
+                let state = board.get(x, y);
+                let count = Self::neighbourhood_plus_center(false, x, y)
+                    .into_iter()
+                    .filter(|&(x, y)| board.get(x, y))
+                    .count();
+                matches!((state, count), (true, 2) | (true, 3) | (false, 3))
+            })
+            .collect();
+        next_board
+    }
+
+    /// Update the state of the game.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use life_backend::{Board, Game};
+    /// let board: Board = [(0, 1), (1, 1), (2, 1)].iter().collect(); // Blinker pattern
+    /// let mut game = Game::new(board);
+    /// game.update();
+    /// let board = game.board();
+    /// assert_eq!(board.bounding_box(), (1, 1, 0, 2));
+    /// assert_eq!(board.get(0, 0), false);
+    /// assert_eq!(board.get(1, 0), true);
+    /// assert_eq!(board.get(2, 0), false);
+    /// assert_eq!(board.get(0, 1), false);
+    /// assert_eq!(board.get(1, 1), true);
+    /// assert_eq!(board.get(2, 1), false);
+    /// assert_eq!(board.get(0, 2), false);
+    /// assert_eq!(board.get(1, 2), true);
+    /// assert_eq!(board.get(2, 2), false);
+    /// ```
+    ///
+    pub fn update(&mut self)
+    where
+        IndexType: Copy + PartialEq + PartialOrd + Add<Output = IndexType> + Sub<Output = IndexType> + One + Bounded + ToPrimitive,
+    {
+        self.board = Self::next_board(self.board())
     }
 }
 
