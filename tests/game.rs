ddf6087c9a8255eb985a60cb882505f4ec49fbea
Masaki Wakabayashi <masaki.wakabayashi@gmail.com>
2023-03-13 12:33:03 +0900

    Separate into the test function and the test pattern.

--- tests/game.rs
+++ tests/game.rs
@@ -1,13 +1,10 @@
 use anyhow::Result;
 use life_backend::{Board, Game};
 
-#[test]
-fn game_glider_test() -> Result<()> {
-    // The initial pattern, steps to be proceed, and the expected final pattern
-    let init_pattern = [(1, 0), (2, 1), (0, 2), (1, 2), (2, 2)]; // Glider pattern
-    let steps = 4;
-    let expected_final_pattern = init_pattern.iter().map(|&(x, y)| (x + 1, y + 1));
+use i16 as I;
 
+// Execute the test with the initial pattern, steps to be proceed, and the expected final pattern.
+fn do_test(init_pattern: &[(I, I)], steps: usize, expected_final_pattern: &[(I, I)]) -> Result<()> {
     // Create the game with the initial pattern
     let board: Board = init_pattern.iter().collect();
     let mut game = Game::new(board);
@@ -24,10 +21,18 @@ fn game_glider_test() -> Result<()> {
     println!("{game}");
 
     // Check the current state of the game
-    let expected_board: Board = expected_final_pattern.collect();
+    let expected_board: Board = expected_final_pattern.iter().collect();
     println!("Expected:");
     println!("(boundary: {:?})", expected_board.bounding_box());
     println!("{expected_board}");
     assert_eq!(*game.board(), expected_board);
     Ok(())
+}
+
+#[test]
+fn game_glider_test() -> Result<()> {
+    let init_pattern = vec![(1, 0), (2, 1), (0, 2), (1, 2), (2, 2)]; // Glider pattern
+    let steps = 4;
+    let expected_final_pattern: Vec<_> = init_pattern.iter().map(|&(x, y)| (x + 1, y + 1)).collect();
+    do_test(&init_pattern, steps, &expected_final_pattern)
 }
