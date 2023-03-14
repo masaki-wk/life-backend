f1bc894f624acc6baacfaa1dfdac86fb615b790e
Masaki Wakabayashi <masaki.wakabayashi@gmail.com>
2023-03-15 06:28:19 +0900

    Add the step_size argument.

--- examples/game.rs
+++ examples/game.rs
@@ -9,7 +9,8 @@ use i16 as I;
 
 struct Config {
     path_str: String,
-    steps: usize,
+    generation: usize,
+    step_size: usize,
 }
 
 impl Config {
@@ -18,14 +19,25 @@ impl Config {
         let Some(path_str) = args.next() else {
             bail!("Not enough arguments");
         };
-        let steps = match args.next() {
+        let generation = match args.next() {
             Some(s) => match s.parse() {
                 Ok(n) => n,
                 Err(_) => bail!("2nd argument is not a number"),
             },
             None => 0,
         };
-        Ok(Self { path_str, steps })
+        let step_size = match args.next() {
+            Some(s) => match s.parse() {
+                Ok(n) => n,
+                Err(_) => bail!("3rd argument is not a number"),
+            },
+            None => 1,
+        };
+        Ok(Self {
+            path_str,
+            generation,
+            step_size,
+        })
     }
 }
 
@@ -35,15 +47,20 @@ fn run(config: Config) -> Result<()> {
     let parser = Plaintext::<I>::new(file)?;
     let board: Board<_> = parser.iter().collect();
     let game = Game::<_>::new(board);
-    simulate(game, config.steps);
+    simulate(game, config.generation, config.step_size);
     Ok(())
 }
 
-fn simulate(mut game: Game<I>, steps: usize) {
-    for i in 0..=steps {
-        if i != 0 {
+fn simulate(mut game: Game<I>, generation: usize, step_size: usize) {
+    let mut i = 0;
+    println!("Generation {i}:");
+    println!("{game}");
+    while i < generation {
+        let steps = step_size.min(generation - i);
+        for _ in 0..steps {
             game.update();
         }
+        i += steps;
         println!("Generation {i}:");
         println!("{game}");
     }
