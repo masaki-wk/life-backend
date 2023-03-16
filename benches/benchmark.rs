4583b7629583264b2f323eca0eea0ed0f4d1a480
Masaki Wakabayashi <masaki.wakabayashi@gmail.com>
2023-03-17 04:38:53 +0900

    Add centinal_1k_benchmark.

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -2,9 +2,11 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use life_backend::format::Plaintext;
 use life_backend::{Board, Game};
 use num_traits::{Bounded, FromPrimitive, One, ToPrimitive, Zero};
+use std::fs::File;
 use std::hash::Hash;
 use std::io::Read;
 use std::ops::{Add, Sub};
+use std::path::Path;
 
 fn workload<IndexType>(board: &Board<IndexType>, steps: usize)
 where
@@ -34,6 +36,15 @@ where
     do_benchmark::<i8, _>(c, id, pattern.as_bytes(), steps);
 }
 
+fn do_benchmark_with_file<IndexType>(c: &mut Criterion, id: &str, path_str: &str, steps: usize)
+where
+    IndexType: Eq + Hash + Copy + PartialOrd + Add<Output = IndexType> + Sub<Output = IndexType> + Zero + One + Bounded + ToPrimitive + FromPrimitive,
+{
+    let path = Path::new(path_str);
+    let file = File::open(path).unwrap();
+    do_benchmark::<i8, _>(c, id, file, steps);
+}
+
 fn blinker_1k_benchmark(c: &mut Criterion) {
     // See: https://conwaylife.com/wiki/Blinker
     let id = "blinker-1k";
@@ -43,6 +54,14 @@ fn blinker_1k_benchmark(c: &mut Criterion) {
     ";
     let steps = 1000;
     do_benchmark_with_string::<i8>(c, id, pattern, steps);
+}
+
+fn centinal_1k_benchmark(c: &mut Criterion) {
+    // See: https://conwaylife.com/wiki/Centinal
+    let id = "centinal-1k";
+    let path_str = concat!(env!("CARGO_MANIFEST_DIR"), "/patterns/centinal.cells");
+    let steps = 1000;
+    do_benchmark_with_file::<i8>(c, id, path_str, steps);
 }
 
 fn moldon30p25_1k_benchmark(c: &mut Criterion) {
@@ -75,5 +94,5 @@ fn moldon30p25_1k_benchmark(c: &mut Criterion) {
     do_benchmark_with_string::<i8>(c, id, pattern, steps);
 }
 
-criterion_group!(benches, blinker_1k_benchmark, moldon30p25_1k_benchmark);
+criterion_group!(benches, blinker_1k_benchmark, centinal_1k_benchmark, moldon30p25_1k_benchmark);
 criterion_main!(benches);
