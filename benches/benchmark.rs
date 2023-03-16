901a1cf1009c5be185a838495a4b198ee5d771c7
Masaki Wakabayashi <masaki.wakabayashi@gmail.com>
2023-03-17 05:31:27 +0900

    Use Result instead of unwrap in internal functions.

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result};
 use criterion::{criterion_group, criterion_main, Criterion};
 use life_backend::format::Plaintext;
 use life_backend::{Board, Game};
@@ -18,31 +19,34 @@ where
     }
 }
 
-fn do_benchmark<IndexType, R>(c: &mut Criterion, id: &str, read: R, steps: usize)
+fn do_benchmark<IndexType, R>(c: &mut Criterion, id: &str, read: R, steps: usize) -> Result<()>
 where
     IndexType: Eq + Hash + Copy + PartialOrd + Add<Output = IndexType> + Sub<Output = IndexType> + Zero + One + Bounded + ToPrimitive + FromPrimitive,
     R: Read,
 {
     let from_usize_unwrap = |x| IndexType::from_usize(x).unwrap();
-    let loader = Plaintext::new(read).unwrap();
+    let loader = Plaintext::new(read)?;
     let board: Board<_> = loader.iter().map(|(x, y)| (from_usize_unwrap(x), from_usize_unwrap(y))).collect();
     c.bench_function(id, |b| b.iter(|| workload(&board, steps)));
+    Ok(())
 }
 
-fn do_benchmark_with_string<IndexType>(c: &mut Criterion, id: &str, pattern: &str, steps: usize)
+fn do_benchmark_with_string<IndexType>(c: &mut Criterion, id: &str, pattern: &str, steps: usize) -> Result<()>
 where
     IndexType: Eq + Hash + Copy + PartialOrd + Add<Output = IndexType> + Sub<Output = IndexType> + Zero + One + Bounded + ToPrimitive + FromPrimitive,
 {
-    do_benchmark::<i8, _>(c, id, pattern.as_bytes(), steps);
+    do_benchmark::<i8, _>(c, id, pattern.as_bytes(), steps)?;
+    Ok(())
 }
 
-fn do_benchmark_with_file<IndexType>(c: &mut Criterion, id: &str, path_str: &str, steps: usize)
+fn do_benchmark_with_file<IndexType>(c: &mut Criterion, id: &str, path_str: &str, steps: usize) -> Result<()>
 where
     IndexType: Eq + Hash + Copy + PartialOrd + Add<Output = IndexType> + Sub<Output = IndexType> + Zero + One + Bounded + ToPrimitive + FromPrimitive,
 {
     let path = Path::new(path_str);
-    let file = File::open(path).unwrap();
-    do_benchmark::<i8, _>(c, id, file, steps);
+    let file = File::open(path).with_context(|| format!("Failed to open \"{}\"", path.display()))?;
+    do_benchmark::<i8, _>(c, id, file, steps)?;
+    Ok(())
 }
 
 fn blinker_1k_benchmark(c: &mut Criterion) {
@@ -53,7 +57,7 @@ fn blinker_1k_benchmark(c: &mut Criterion) {
         OOO\n\
     ";
     let steps = 1000;
-    do_benchmark_with_string::<i8>(c, id, pattern, steps);
+    do_benchmark_with_string::<i8>(c, id, pattern, steps).unwrap();
 }
 
 fn centinal_1k_benchmark(c: &mut Criterion) {
@@ -61,7 +65,7 @@ fn centinal_1k_benchmark(c: &mut Criterion) {
     let id = "centinal-1k";
     let path_str = concat!(env!("CARGO_MANIFEST_DIR"), "/patterns/centinal.cells");
     let steps = 1000;
-    do_benchmark_with_file::<i8>(c, id, path_str, steps);
+    do_benchmark_with_file::<i8>(c, id, path_str, steps).unwrap();
 }
 
 fn moldon30p25_1k_benchmark(c: &mut Criterion) {
@@ -69,7 +73,7 @@ fn moldon30p25_1k_benchmark(c: &mut Criterion) {
     let id = "moldon30p25-1k";
     let path_str = concat!(env!("CARGO_MANIFEST_DIR"), "/patterns/moldon30p25.cells");
     let steps = 1000;
-    do_benchmark_with_file::<i8>(c, id, path_str, steps);
+    do_benchmark_with_file::<i8>(c, id, path_str, steps).unwrap();
 }
 
 criterion_group!(benches, blinker_1k_benchmark, centinal_1k_benchmark, moldon30p25_1k_benchmark);
