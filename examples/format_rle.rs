faf5a04a72ec6d091321ebef07c8757744dde346
Masaki Wakabayashi <masaki.wakabayashi@gmail.com>
2023-03-31 13:12:19 +0900

    Renew examples/format_rle.rs as a loader program.

--- examples/format_rle.rs
+++ examples/format_rle.rs
@@ -1,16 +1,32 @@
-use anyhow::Result;
+use anyhow::{bail, Context, Result};
 use life_backend::format::Rle;
+use std::env;
+use std::fs::File;
+use std::path::Path;
+
+struct Config {
+    path_str: String,
+}
+
+impl Config {
+    fn new(mut args: env::Args) -> Result<Self> {
+        args.next();
+        let Some(path_str) = args.next() else {
+            bail!("Not enough arguments");
+        };
+        Ok(Self { path_str })
+    }
+}
+
+fn run(config: Config) -> Result<()> {
+    let path = Path::new(&config.path_str);
+    let file = File::open(path).with_context(|| format!("Failed to open \"{}\"", path.display()))?;
+    let parser = Rle::new(file)?;
+    println!("{parser}");
+    Ok(())
+}
 
 fn main() -> Result<()> {
-    let pattern = "\
-        #N Glider\n\
-        x = 3, y = 3\n\
-        bo$2bo$3o!\n\
-    ";
-    let parser = Rle::new(pattern.as_bytes())?;
-    println!("{:?}", parser);
-    println!("{parser}");
-    let v = parser.iter().collect::<Vec<_>>();
-    println!("{:?}", v);
-    Ok(())
+    let config = Config::new(env::args())?;
+    run(config)
 }
