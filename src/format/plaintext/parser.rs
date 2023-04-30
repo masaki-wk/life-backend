6435add203fe115c0be1c412e67a803df1158dd0
Masaki Wakabayashi <masaki.wakabayashi@gmail.com>
2023-04-30 12:44:13 +0900

    Separate format::Plaintext into submodules.

---
+++ src/format/plaintext/parser.rs
@@ -0,0 +1,74 @@
+use anyhow::{anyhow, Result};
+
+use super::PlaintextLine;
+
+// An internal struct, used during constructing of Plaintext
+pub(super) struct PlaintextParser {
+    pub(super) name: Option<String>,
+    pub(super) comments: Vec<String>,
+    pub(super) lines: usize,
+    pub(super) contents: Vec<PlaintextLine>,
+}
+
+// Inherent methods
+
+impl PlaintextParser {
+    fn parse_prefixed_line<'a>(prefix: &str, line: &'a str) -> Option<&'a str> {
+        if line.len() < prefix.len() {
+            None
+        } else {
+            let (first, last) = line.split_at(prefix.len());
+            if first == prefix {
+                Some(last)
+            } else {
+                None
+            }
+        }
+    }
+    #[inline]
+    fn parse_name_line(line: &str) -> Option<&str> {
+        Self::parse_prefixed_line("!Name: ", line)
+    }
+    #[inline]
+    fn parse_comment_line(line: &str) -> Option<&str> {
+        Self::parse_prefixed_line("!", line)
+    }
+    fn parse_content_line(line: &str) -> Result<Vec<usize>> {
+        line.chars()
+            .enumerate()
+            .filter_map(|(i, c)| match c {
+                '.' => None,
+                'O' => Some(Ok(i)),
+                _ => Some(Err(anyhow!("Invalid character found in the pattern"))),
+            })
+            .collect()
+    }
+    pub(super) fn new() -> Self {
+        Self {
+            name: None,
+            comments: Vec::new(),
+            lines: 0,
+            contents: Vec::new(),
+        }
+    }
+    pub(super) fn push(&mut self, line: &str) -> Result<()> {
+        if self.name.is_none() && self.comments.is_empty() && self.lines == 0 {
+            if let Some(name) = Self::parse_name_line(line) {
+                self.name = Some(name.to_string());
+                return Ok(());
+            }
+        }
+        if self.lines == 0 {
+            if let Some(comment) = Self::parse_comment_line(line) {
+                self.comments.push(comment.to_string());
+                return Ok(());
+            }
+        }
+        let content = Self::parse_content_line(line)?;
+        if !content.is_empty() {
+            self.contents.push(PlaintextLine(self.lines, content));
+        }
+        self.lines += 1;
+        Ok(())
+    }
+}
---
+++ src/format/plaintext/tests.rs
@@ -0,0 +1,211 @@
+use anyhow::Result;
+
+use super::*;
+
+fn do_check(target: &Plaintext, expected_name: &Option<&str>, expected_comments: &[&str], expected_contents: &[PlaintextLine]) {
+    let expected_name = expected_name.map(String::from);
+    assert_eq!(target.name(), expected_name);
+    assert_eq!(target.comments().len(), expected_comments.len());
+    for (result, expected) in target.comments().iter().zip(expected_comments.iter()) {
+        assert_eq!(result, expected);
+    }
+    assert_eq!(target.contents.len(), expected_contents.len());
+    for (result, expected) in target.contents.iter().zip(expected_contents.iter()) {
+        assert_eq!(result, expected);
+    }
+}
+
+fn do_new_test_to_be_passed(pattern: &str, expected_name: &Option<&str>, expected_comments: &[&str], expected_contents: &[PlaintextLine]) -> Result<()> {
+    let target = Plaintext::new(pattern.as_bytes())?;
+    do_check(&target, expected_name, expected_comments, expected_contents);
+    assert_eq!(target.to_string(), pattern);
+    Ok(())
+}
+
+fn do_new_test_to_be_failed(pattern: &str) {
+    let target = Plaintext::new(pattern.as_bytes());
+    assert!(target.is_err());
+}
+
+#[test]
+fn test_new_empty() -> Result<()> {
+    let pattern = "";
+    let expected_name = None;
+    let expected_comments = Vec::new();
+    let expected_contents = Vec::new();
+    do_new_test_to_be_passed(pattern, &expected_name, &expected_comments, &expected_contents)
+}
+
+#[test]
+fn test_new_header() -> Result<()> {
+    let pattern = "!Name: test\n";
+    let expected_name = Some("test");
+    let expected_comments = Vec::new();
+    let expected_contents = Vec::new();
+    do_new_test_to_be_passed(pattern, &expected_name, &expected_comments, &expected_contents)
+}
+
+#[test]
+fn test_new_no_header_but_comment() -> Result<()> {
+    let pattern = "!comment\n";
+    let expected_name = None;
+    let expected_comments = vec!["comment"];
+    let expected_contents = Vec::new();
+    do_new_test_to_be_passed(pattern, &expected_name, &expected_comments, &expected_contents)
+}
+
+#[test]
+fn test_new_header_comment() -> Result<()> {
+    let pattern = concat!("!Name: test\n", "!comment\n");
+    let expected_name = Some("test");
+    let expected_comments = vec!["comment"];
+    let expected_contents = Vec::new();
+    do_new_test_to_be_passed(pattern, &expected_name, &expected_comments, &expected_contents)
+}
+
+#[test]
+fn test_new_header_comments() -> Result<()> {
+    let pattern = concat!("!Name: test\n", "!comment0\n", "!comment1\n");
+    let expected_name = Some("test");
+    let expected_comments = vec!["comment0", "comment1"];
+    let expected_contents = Vec::new();
+    do_new_test_to_be_passed(pattern, &expected_name, &expected_comments, &expected_contents)
+}
+
+#[test]
+fn test_new_header_content() -> Result<()> {
+    let pattern = concat!("!Name: test\n", ".O\n");
+    let expected_name = Some("test");
+    let expected_comments = Vec::new();
+    let expected_contents = vec![PlaintextLine(0, vec![1])];
+    do_new_test_to_be_passed(pattern, &expected_name, &expected_comments, &expected_contents)
+}
+
+#[test]
+fn test_new_header_contents() -> Result<()> {
+    let pattern = concat!("!Name: test\n", ".O\n", "O.\n");
+    let expected_name = Some("test");
+    let expected_comments = Vec::new();
+    let expected_contents = vec![PlaintextLine(0, vec![1]), PlaintextLine(1, vec![0])];
+    do_new_test_to_be_passed(pattern, &expected_name, &expected_comments, &expected_contents)
+}
+
+#[test]
+fn test_new_header_comments_contents() -> Result<()> {
+    let pattern = concat!("!Name: test\n", "!comment0\n", "!comment1\n", ".O\n", "O.\n");
+    let expected_name = Some("test");
+    let expected_comments = vec!["comment0", "comment1"];
+    let expected_contents = vec![PlaintextLine(0, vec![1]), PlaintextLine(1, vec![0])];
+    do_new_test_to_be_passed(pattern, &expected_name, &expected_comments, &expected_contents)
+}
+
+#[test]
+fn test_new_wrong_header() {
+    let pattern = "_\n";
+    do_new_test_to_be_failed(pattern)
+}
+
+#[test]
+fn test_new_duplicate_header() -> Result<()> {
+    let pattern = concat!("!Name: name0\n", "!Name: name1\n");
+    let expected_name = Some("name0");
+    let expected_comments = vec!["Name: name1"];
+    let expected_contents = Vec::new();
+    do_new_test_to_be_passed(pattern, &expected_name, &expected_comments, &expected_contents)
+}
+
+#[test]
+fn test_new_wrong_content_without_comment() {
+    let pattern = concat!("!Name: test\n", "_\n");
+    do_new_test_to_be_failed(pattern)
+}
+
+#[test]
+fn test_new_wrong_content_with_comment() {
+    let pattern = concat!("!Name: test\n", "!\n", "_\n");
+    do_new_test_to_be_failed(pattern)
+}
+
+#[test]
+fn test_build() -> Result<()> {
+    let pattern = [(1, 0), (0, 1)];
+    let expected_name = None;
+    let expected_comments = Vec::new();
+    let expected_contents = vec![PlaintextLine(0, vec![1]), PlaintextLine(1, vec![0])];
+    let target = pattern.iter().collect::<PlaintextBuilder>().build()?;
+    do_check(&target, &expected_name, &expected_comments, &expected_contents);
+    Ok(())
+}
+
+#[test]
+fn test_build_singleline_name() -> Result<()> {
+    let pattern = [(1, 0), (0, 1)];
+    let expected_name = Some("test");
+    let expected_comments = Vec::new();
+    let expected_contents = vec![PlaintextLine(0, vec![1]), PlaintextLine(1, vec![0])];
+    let target = pattern.iter().collect::<PlaintextBuilder>().name("test").build()?;
+    do_check(&target, &expected_name, &expected_comments, &expected_contents);
+    Ok(())
+}
+
+#[test]
+fn test_build_blank_name() -> Result<()> {
+    let pattern = [(1, 0), (0, 1)];
+    let expected_name = Some("");
+    let expected_comments = Vec::new();
+    let expected_contents = vec![PlaintextLine(0, vec![1]), PlaintextLine(1, vec![0])];
+    let target = pattern.iter().collect::<PlaintextBuilder>().name("").build()?;
+    do_check(&target, &expected_name, &expected_comments, &expected_contents);
+    Ok(())
+}
+
+#[test]
+fn test_build_multiline_name() {
+    let pattern = [(1, 0), (0, 1)];
+    let target = pattern.iter().collect::<PlaintextBuilder>().name("name\nname").build();
+    assert!(target.is_err());
+}
+
+#[test]
+fn test_build_comment() -> Result<()> {
+    let pattern = [(1, 0), (0, 1)];
+    let expected_name = None;
+    let expected_comments = vec!["comment"];
+    let expected_contents = vec![PlaintextLine(0, vec![1]), PlaintextLine(1, vec![0])];
+    let target = pattern.iter().collect::<PlaintextBuilder>().comment("comment").build()?;
+    do_check(&target, &expected_name, &expected_comments, &expected_contents);
+    Ok(())
+}
+
+#[test]
+fn test_build_blank_comment() -> Result<()> {
+    let pattern = [(1, 0), (0, 1)];
+    let expected_name = None;
+    let expected_comments = vec![""];
+    let expected_contents = vec![PlaintextLine(0, vec![1]), PlaintextLine(1, vec![0])];
+    let target = pattern.iter().collect::<PlaintextBuilder>().comment("").build()?;
+    do_check(&target, &expected_name, &expected_comments, &expected_contents);
+    Ok(())
+}
+
+#[test]
+fn test_build_comments() -> Result<()> {
+    let pattern = [(1, 0), (0, 1)];
+    let expected_name = None;
+    let expected_comments = vec!["comment0", "comment1"];
+    let expected_contents = vec![PlaintextLine(0, vec![1]), PlaintextLine(1, vec![0])];
+    let target = pattern.iter().collect::<PlaintextBuilder>().comment("comment0\ncomment1").build()?;
+    do_check(&target, &expected_name, &expected_comments, &expected_contents);
+    Ok(())
+}
+
+#[test]
+fn test_build_name_comment() -> Result<()> {
+    let pattern = [(1, 0), (0, 1)];
+    let expected_name = Some("test");
+    let expected_comments = vec!["comment"];
+    let expected_contents = vec![PlaintextLine(0, vec![1]), PlaintextLine(1, vec![0])];
+    let target = pattern.iter().collect::<PlaintextBuilder>().name("test").comment("comment").build()?;
+    do_check(&target, &expected_name, &expected_comments, &expected_contents);
+    Ok(())
+}
