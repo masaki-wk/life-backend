9c68ff28f889966733bd7e290c5ccc78b6cabc0e
Masaki Wakabayashi <masaki.wakabayashi@gmail.com>
2023-05-18 07:35:34 +0900

    Refne the doc test of Plaintext::live_cells().

--- src/format/plaintext/core.rs
+++ src/format/plaintext/core.rs
@@ -109,12 +109,7 @@ impl Plaintext {
     ///     .O.\n\
     /// ";
     /// let parser = Plaintext::new(pattern.as_bytes())?;
-    /// let mut iter = parser.live_cells();
-    /// assert_eq!(iter.next(), Some((0, 0)));
-    /// assert_eq!(iter.next(), Some((1, 0)));
-    /// assert_eq!(iter.next(), Some((2, 0)));
-    /// assert_eq!(iter.next(), Some((1, 1)));
-    /// assert_eq!(iter.next(), None);
+    /// assert!(parser.live_cells().eq([(0, 0), (1, 0), (2, 0), (1, 1)]));
     /// # Ok(())
     /// # }
     /// ```
